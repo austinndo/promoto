02980fb1697a8b79e385c2ede3b47dec05a9285e
Austin Do <austinndo@gmail.com>
2022-07-25 15:22:54 -0700

    At least one of each CRUD route working. Trying out updates to genre array only for Song data

---
+++ controllers/index.js
@@ -0,0 +1,175 @@
+const Song = require('../models/song')
+const Post = require('../models/post')
+
+////---------------------------- Songs ----------------------------////
+
+//------------ Create ------------//
+
+//How it was done in the review
+// const createSongg = async (req, res) => {
+//   try {
+//     const newSong = await Song.create(req.body)
+//     await newSong.save()
+//     return res.status(201).json({ newSong })
+//      ||
+//     res.send(newSong)
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+const createSong = async (req, res) => {
+  try {
+    const newSong = await new Song(req.body)
+    await newSong.save()
+    return res.status(201).json({ newSong })
+  } catch (error) {
+    throw error
+  }
+}
+
+//------------ Read ------------//
+
+const getSongs = async (req, res) => {
+  try {
+    const songs = await Song.find()
+    return res.status(200).json({ songs })
+  } catch (error) {
+    throw error
+  }
+}
+
+const getSongById = async (req, res) => {
+  try {
+    const song = await Song.find({ id: req.params.id })
+    return res.status(200).json({ song })
+  } catch (error) {
+    throw error
+  }
+}
+
+//------------ Update ------------//
+
+//------------ Delete ------------//
+
+////---------------------------- Posts ----------------------------////
+
+//---------------- Create ----------------//
+
+//---------------- Read ----------------//
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+    return res.status(200).json({ posts })
+  } catch (error) {
+    throw error
+  }
+}
+
+const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    return res.status(200).json({ post })
+  } catch (error) {
+    throw error
+  }
+}
+
+//---------------- Update ----------------//
+
+//consider using the ids saved in the database we created manually
+const updateSong = async (req, res) => {
+  try {
+    const songId = req.params.objectId
+    const updatedSong = await Song.updateOne(req.body, {
+      where: { _id: songId },
+      returning: true
+    })
+    res.send(updatedSong)
+  } catch (error) {
+    throw error
+  }
+}
+
+const updateGenres = async (req, res) => {
+  try {
+    const newGenre = req.body.genre
+    const songId = req.params.objectId
+    const updatedSong = await Song.update(
+      { _id: songId },
+      { $push: { genre: { newGenre } } }
+    )
+    res.send(updatedSong)
+  } catch (error) {
+    throw error
+  }
+}
+
+const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.objectId
+    const updatedPost = await Post.update(req.body, {
+      where: { _id: postId },
+      returning: true
+    })
+    res.send(updatedPost)
+  } catch (error) {
+    throw error
+  }
+}
+
+//---------------- Delete ----------------//
+
+// const deletePost = async (req, res) => {
+//   try {
+//     const postId = req.params.objectId
+//     await Post.findByIdAndDelete({ where: { _id: postId } })
+//     res.send({ msg: `Post of id: ${postId} deleted` })
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Post.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Post deleted')
+    }
+    throw new Error('Post not found')
+  } catch (error) {
+    throw error
+  }
+}
+
+////////-------- Module Exports --------////////
+
+module.exports = {
+  createSong,
+  getSongs,
+  getSongById,
+  getPosts,
+  getPostById,
+  updatePost,
+  updateSong,
+  updateGenres,
+  deletePost
+}
+
+//////----------Notes from Router and Controller Review------------////////////
+
+//create Index / List route
+//getObjects
+
+//create Details / Show route
+//getObjectById
+// const getObjectById = async (req, res) => {
+//   try {
+//     const object = await Object.findById(req.params.id)
+// return res.status(200).json({object}) || res.send(object)
+//   } catch (error) {
+//     throw error
+//   }
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,34 @@
 const { Router } = require('express')
 const router = Router()
+const controllers = require('../controllers')
+
+////////-------- Create --------////////
+
+router.post('/new-song', controllers.createSong)
+
+////////-------- Read --------////////
 
 router.get('/', (req, res) => res.send('Arrived at the Promoto root!'))
+
+router.get('/songs', controllers.getSongs)
+
+router.get('/song/id/:id', controllers.getSongById)
+
+router.get('/posts', controllers.getPosts)
+
+router.get('/post/:id', controllers.getPostById)
+
+////////-------- Update --------////////
+//put vs patch ??
+
+router.put('/post/edit/:id', controllers.updatePost)
+
+router.put('/song/edit/:id', controllers.updateSong)
+
+router.put('/song/genres/:id', controllers.updateGenres)
+
+////////-------- Delete --------////////
+
+router.delete('/post/:id', controllers.deletePost)
 
 module.exports = router
