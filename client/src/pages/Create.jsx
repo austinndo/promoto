69ae43a0f868b9f185df5b2e9dbbb14a02117490
Austin Do <austinndo@gmail.com>
2022-07-29 07:43:33 -0700

    Got rid of a few posts. Touched up styling just a bit

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -106,7 +106,7 @@ const Create = () => {
       description: '',
       likes: 0 })
 
-    navigate('/')
+    navigate('/promoto/search/posts')
   }
 
   //------------------------ Form handlers ------------------------//
