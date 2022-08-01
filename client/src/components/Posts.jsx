c88e8edbccbdb625c631609ff9f4e5aa5184108b
Austin Do <austinndo@gmail.com>
2022-07-31 22:04:27 -0700

    Like button working! Need to update code to display patch without having to reload the page first

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -13,7 +13,13 @@ const Post = (props) => {
       <img className="postContainerImage" src={post.image} alt="post-content"/>
       <div className="postContainerUsernameLikes">
       <h4 className="postUsername">{post.username}</h4>
-      <h5 className="postLikes">{post.likes}<i class="fa fa-thumbs-up" aria-hidden="true"></i></h5>
+      <h5 className="postLikes">{post.likes}<i class="fa fa-thumbs-up" aria-hidden="true" style={{cursor:"pointer"}} onClick={async () => {
+        await axios({
+          method: 'patch',
+          url: `http://localhost:3001/post/edit/${post._id}`,
+          data: {likes: post.likes + 1}
+          })}}>
+      </i></h5>
       </div>
       <h5 className="postDescription">{post.description}</h5>
       <button onClick={async () => {
@@ -28,3 +34,4 @@ const Post = (props) => {
 }
 
 export default Post
+
