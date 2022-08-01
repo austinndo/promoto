3bbe3dceec43bc9cff36639d0999a62a0c1a9b89
Austin Do <austinndo@gmail.com>
2022-08-01 10:33:18 -0700

    Setting up heroku app mighty-dusk

--- db/index.js
+++ db/index.js
@@ -11,7 +11,7 @@ let dbUrl =
 
 mongoose
   //previous .connect(MONGODB_URI, { ... })
-  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to Promoto MongoDB.')
   })
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 const db = require('./db')
-const cors = require('cors')
-
 const PORT = process.env.PORT || 3001
-
 const app = express()
 
-app.use(express.json())
-
 app.use(cors())
+app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes)
-
-app.use(express.static(`${__dirname}/client/build`))
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname / client / build / indexedDB.html}`)
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
