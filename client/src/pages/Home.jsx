30e443a7cf19567aaf2949c776de62702a2cedf6
Austin Do <austinndo@gmail.com>
2022-10-04 11:30:12 -0700

    Trying with new env variable

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const posts = await axios.get('http://localhost:3001/posts')
+      const posts = await axios.get('REACT_APP_MONGODB_URI/posts')
       setPosts(posts.data.posts.reverse())
     }
     getPosts()
