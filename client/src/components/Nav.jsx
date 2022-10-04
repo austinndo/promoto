7298cb220a02f5f91a6460ec2ed3d73c79441384
Austin Do <austinndo@gmail.com>
2022-10-04 10:39:22 -0700

    Changed nav colors and style

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,11 +8,11 @@ const Nav = () => {
         <h2>Promoto</h2>
       </div>
       <div className='navbarLinks'>
-        <Link to="/promoto" alt="Home"><i class="fa fa-home fa-2x"></i></Link>
-        <Link to="/promoto/search" alt="Search"><i class="fa fa-search fa-2x" aria-hidden="true"></i></Link>
-        <Link to="/promoto/spotlight" alt="Spotlight"><i class="fa fa-calendar fa-2x" aria-hidden="true"></i></Link>
-        <Link to="/promoto/add" alt="Add/Create"><i class="fa fa-plus-square fa-2x" aria-hidden="true"></i></Link>
-        <Link to="/promoto/edit" alt="Edit"><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></Link>
+        <Link to="/promoto" alt="Home" className='navLink'><i class="fa fa-home fa-2x"></i></Link>
+        <Link to="/promoto/search" alt="Search" className='navLink'><i class="fa fa-search fa-2x" aria-hidden="true"></i></Link>
+        <Link to="/promoto/spotlight" alt="Spotlight" className='navLink'><i class="fa fa-calendar fa-2x" aria-hidden="true"></i></Link>
+        <Link to="/promoto/add" alt="Add/Create" className='navLink'><i class="fa fa-plus-square fa-2x" aria-hidden="true"></i></Link>
+        <Link to="/promoto/edit" alt="Edit" className='navLink'><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></Link>
       </div>
     </div>
   )
