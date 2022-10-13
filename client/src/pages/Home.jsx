0b611e03cd60b42fdfce3e63724de217c7f5be43
Austin Do <austinndo@gmail.com>
2022-10-12 17:48:30 -0700

    Testing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const posts = await axios.get('http://localhost:3001/posts')
+      const posts = await axios.get('https://data.mongodb-api.com/app/data-mdtef/endpoint/data/v1/posts')
       setPosts(posts.data.posts.reverse())
     }
     getPosts()
